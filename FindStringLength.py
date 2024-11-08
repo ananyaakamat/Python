58e27b2e33156069e7ef70d90e5d9143be9e8089
DESKTOP-J36A1MM\Anaconda <anant.kamat.in@gmail.com>
2024-11-08 10:40:43 +0530

    Added Find string length script

---
+++ FindStringLength.py
@@ -0,0 +1,8 @@
+# Ask the user to input a string
+user_input = input("Enter a string: ")
+
+# Calculate the length of the input string using the len() function
+string_length = len(user_input)
+
+# Print the length of the input string
+print(f"The length of the entered string is: {string_length}")
